6339a73295d217de9d630d120adde411833cd6e1
Yohandys <ymtnez@upr.edu.cu>
2023-04-16 14:17:30 -0500

    Agregando tres useEffect en SimpleForm.js

--- src/components/02-useEffect/SimpleForm.js
+++ src/components/02-useEffect/SimpleForm.js
@@ -11,21 +11,38 @@ export const SimpleForm = () => {
     );
 
     const { nombre, correo } = formState;
-
+    
+    /**
+     * Se recomienda que los efectos los utilicemos de manera individual para
+     * cada cosa que cambie, ejemplo: los estados, el formulario, etc.
+     */
     useEffect(() => {
-        console.log('first');    
-    }, [])
+        console.log('Primer cambio');    
+    }, []);
+
+    //Este efecto se disparara cuando cambie el formulario
+    useEffect(() => {
+        console.log('Cambio el form');    
+    }, [ formState ]);
+
+    //Este efecto se disparara cuando cambie el correo en el formulario
+    useEffect(() => {
+        console.log('Cambio el correo');    
+    }, [ correo ]);
 
     const handleInputChange = ({ target }) => {
-        console.log( target )
+        
         setFormState(
             {
                 ...formState,
                 /**
-                 * Con esto computamos o cambiamos el nombre de la propiedad
-                 * por lo que venga en el target.value es decir, si en el value
-                 * viene
-                 * 
+                 * Con esto computamos o cambiamos el nombre de la propiedad del
+                 * objeto (nombre o correo) por el que viene de la propiedad name
+                 * del target del input. Es decir, si escribi en el input que tiene
+                 * como name='correo' entonces target.name es correo y a este le
+                 * asignamos el target.value. Si escribi en el input del nombre
+                 * entonces en target.name sera nombre y su valor lo que venga
+                 * en target.value. 
                  */
                 [target.name]: target.value
             }
