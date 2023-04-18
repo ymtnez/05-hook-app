46940c5c4841de62892fe652b79298de1fc21d32
Yohandys <ymtnez@upr.edu.cu>
2023-04-18 15:08:18 -0500

    Creando la carpeta 03-examples con el js y css de MultipleCustomHook, ademas del customHook useFetch de hook

---
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './multCustHook.css'
+
+export const MultipleCustomHooks = () => {
+    return (
+        <div>
+            <h1>Custom Hook!!!</h1> 
+        </div>
+    )
+}
